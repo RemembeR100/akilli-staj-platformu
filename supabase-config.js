b9302ae99989b06a8dc4631f07f887f11d2d9a44
RemembeR100 <sametakgeyik18@icloud.com>
2026-06-02 17:05:16 +0300

    son güncelleme

--- supabase-config.js
+++ supabase-config.js
@@ -10,4 +10,4 @@ const SUPABASE_URL = 'https://xybnhxrxvvbczuinnska.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5Ym5oeHJ4dnZiY3p1aW5uc2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MDI1NDgsImV4cCI6MjA5NTk3ODU0OH0.zVafRGKvmuA_UiqqcRnVEN09raMP97nWsFniAbRMqZo';
 
 // Supabase Client oluştur
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
